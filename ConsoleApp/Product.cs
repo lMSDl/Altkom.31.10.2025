ebdf83879877624ec77c785395d7827dd3976751
Paweł <pawel@msd.info.pl>
2025-10-31 10:39:41 +0100

    EditMode

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+
+//funkcja wygenerowana na podstawie nagłówka funkcji
+int Sum(float a, float b) //Tokenizacja: int, Sum, (, float, a, ,, float, b, )
+    => (int)(a + b);
+
+
+//funkcja wygenerowana na podstawie komentarza
+//substract two float numbers and return an integer. Use block body syntax.
+int Subtract(float a, float b)
+{
+    return (int)(a - b);
+}
+
+
+//multiply two numebrs
+int Multiply(float a, float b)
+{
+    return (int)(a * b);
+}
+
+//pierwotnie wygenerowana funkcja Multiply została zmodyfikowana przez użytkownika
+//co spwodowało, że model dostosował kolejny generowany kod (Divide) do zmian użytkownika
+//kontenst "nauki" copilot jest ograniczony do bieżącej sesji - np. wyłączenie IDE powoduje utratę kontekstu
+
+//divide two numbers
+int Divide(float a, float b) 
+{
+    if (b == 0)
+    {
+        throw new DivideByZeroException("Denominator cannot be zero.");
+    }
+    return (int)(a / b);
+}
+
+
+Point3D CreatePoint3D(float[] tab)
+{
+    if (tab.Length < 3)
+    {
+        throw new ArgumentException("Array must contain at least three elements.");
+    }
+    return new Point3D(tab[0], tab[1], tab[2]);
+}
